cd30de089f7499b1f03925e4a6046ffbbf3286bd
Tegar Wijaya Kusuma <tegarwijayakusuma2004@gmail.com>
2025-01-29 09:52:16 +0700

    Add view for creating new products

--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
-def home(request):  
-    return render(request, 'inventory/home.html')
+def product_list(request):  
+    products = Product.objects.all()  
+    return render(request, 'inventory/product_list.html', {'products': products})
+
+# Add this new function for creating products
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+        else:
+            form = ProductForm()
+        return render(request, 'inventory/add_product.html', {'form': form})
