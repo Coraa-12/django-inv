a623a8c3857d3d8fc66a564a6c914eaa93b8bb97
Tegar Wijaya Kusuma <tegarwijayakusuma2004@gmail.com>
2025-01-29 09:54:52 +0700

    Add URL route for product creation form

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views  
 
 urlpatterns = [  
-    path('', views.home, name='home'),  
-]  
+    path('products/', views.product_list, name='product_list'),  
+    path('products/add/', views.add_product, name='add_product'),
+]
